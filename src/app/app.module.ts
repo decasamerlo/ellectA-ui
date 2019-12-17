94c24e3c90a9ec95f9d21ac38837f7cae8d4da8a
Andre Merlo <casagrandamerlo@gmail.com>
2019-12-17 16:13:28 -0300

    criado componente de eleicao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
+import { EleicoesCadastroComponent } from './eleicoes/eleicoes-cadastro/eleicoes-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EleicoesCadastroComponent
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    BrowserAnimationsModule,
+
+    ButtonModule,
+    InputTextModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/eleicoes/eleicoes-cadastro/eleicoes-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eleicoes-cadastro',
+  templateUrl: './eleicoes-cadastro.component.html',
+  styleUrls: ['./eleicoes-cadastro.component.css']
+})
+export class EleicoesCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
