98a34bd6d5bb7aa08b6b8d1fcc0e9243c4db7099
André Merlo <casagrandamerlo@gmail.com>
2019-12-18 10:07:44 -0300

    criado módulo shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +14,8 @@ import { EleicoesModule } from './eleicoes/eleicoes.module';
     BrowserModule,
     BrowserAnimationsModule,
 
-    EleicoesModule
+    EleicoesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
