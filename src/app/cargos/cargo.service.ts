2420c65e143bdfac7def49010f42741546fbf197
Andre Merlo <casagrandamerlo@gmail.com>
2019-12-20 01:43:17 -0300

    criado serviço de cargos

---
+++ src/app/cargos/cargo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Cargo } from './cargo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CargoService {
+
+  cargosUrl = 'http://localhost:8080/cargos';
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Promise<any> {
+    return this.http.get(`${this.cargosUrl}`).toPromise();
+  }
+}
--- src/app/cargos/cargos.module.ts
+++ src/app/cargos/cargos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -11,6 +12,7 @@ import { CargosRoutingModule } from './cargos-routing.model';
   declarations: [CargosCadastroComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
 
     ButtonModule,
     InputTextModule,
