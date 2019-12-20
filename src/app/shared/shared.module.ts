1266f9519bd6b9b195fcb96bd98d4e295ee70c55
André Merlo <casagrandamerlo@gmail.com>
2019-12-20 09:20:59 -0300

    criado componente para validação de erros nos formulários

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,14 +5,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { ErrorHandlerService } from './error-handler.service';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
-  declarations: [NavbarComponent, PaginaNaoEncontradaComponent],
+  declarations: [NavbarComponent, PaginaNaoEncontradaComponent, MessageComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, MessageComponent],
   providers: [ErrorHandlerService]
 })
 export class SharedModule { }
