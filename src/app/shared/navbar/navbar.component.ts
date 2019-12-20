7221baef45bdf06b3da395d8b82bee4796f0b925
André Merlo <casagrandamerlo@gmail.com>
2019-12-20 12:01:55 -0300

    ajustada navegação da barra lateral

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   exibindoMenu = false;
-  menu = 'cadastro';
+  menu = 'login';
 
   constructor() { }
 
